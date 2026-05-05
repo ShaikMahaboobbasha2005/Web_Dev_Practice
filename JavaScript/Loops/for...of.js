b731123d9528edbfafef53a7845abf5b047ab59c
ShaikMahaboobbasha2005 <smehaboobbasha2005@gmail.com>
2026-05-05 22:56:40 +0530

    Add Loops section under JavaScript

---
+++ JavaScript/Loops/for...of.js
@@ -0,0 +1,10 @@
+const names = [
+    ["Basha","Tabrez","Basheer"],
+    ["shiva","Hemanth","Subahan"],
+    ["Rohit","Suresh","Rakesh"]
+];
+for(let row of names){
+    for(let name of row){
+        console.log(name);
+    }
+}
