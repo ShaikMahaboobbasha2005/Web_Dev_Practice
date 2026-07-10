03c696f51eb060dba68aa04fb15080c4aac2085a
ShaikMahaboobbasha2005 <smehaboobbasha2005@gmail.com>
2026-07-10 23:37:41 +0530

    Add object literals practice exercises

---
+++ JavaScript/3.ObjectLiterals/accessol.js
@@ -0,0 +1,7 @@
+const car={
+    brand:"Toyota",
+    model:"Fortuner",
+    year : 2020
+};
+const output = ``
+console.log(`${car.key} ${car.value}`)
---
+++ JavaScript/3.ObjectLiterals/updateol.js
@@ -0,0 +1,10 @@
+const laptop= {
+    brand : "HP",
+    model : "Pavillion",
+    price : 60000
+};
+console.log(laptop)
+laptop.price= 65000;
+console.log(laptop)
+laptop.color = "black";
+console.log(laptop)
